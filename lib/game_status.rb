5c65c22ec618c6e638a6da5b7e142221d0e3455c
Raul Sanchez <rsanchez_99@msn.com>
2018-02-08 20:29:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win|
-     top_row_win = win[0]
+     top_row_win = win[0][0]
     # position_2 = board[win[1]]
     # position_3 = board[win[2]]
     # position_4 = board[win[3]]
